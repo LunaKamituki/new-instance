8893d116a135b046e8edf1b667ee7cb24291d17f
siawaseok3 <siawaseok@outlook.jp>
2024-08-21 00:15:45 +0900

    script.js を更新

---
+++ blog/script.js
@@ -0,0 +1,32 @@
+document.getElementById('accept-btn').addEventListener('click', function() {
+    document.cookie = "yuki=True; max-age=31536000; path=/";
+    location.href = "/";
+    return;
+});
+
+document.getElementById('reject-btn').addEventListener('click', function() {
+    hideCookieNotice();
+    localStorage.setItem('cookieAccepted', 'false');
+});
+
+window.onload = function() {
+    const cookieAccepted = localStorage.getItem('cookieAccepted');
+    if (cookieAccepted !== 'true' && cookieAccepted !== 'false') {
+        showCookieNotice();
+    }
+};
+
+function showCookieNotice() {
+    const cookieNotice = document.getElementById('cookie-notice');
+    cookieNotice.style.opacity = '1';
+    cookieNotice.style.transform = 'translateY(0)';
+}
+
+function hideCookieNotice() {
+    const cookieNotice = document.getElementById('cookie-notice');
+    cookieNotice.style.opacity = '0';
+    cookieNotice.style.transform = 'translateY(20px)';
+    setTimeout(() => {
+        cookieNotice.style.display = 'none';
+    }, 500);
+}
