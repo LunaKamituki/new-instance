a914f7c052f5b7be25b1eb0fc4aa3dfd9bf3fbb9
siawaseok3 <siawaseok@outlook.jp>
2024-08-21 01:26:24 +0900

    main.py を更新

--- main.py
+++ main.py
@@ -180,7 +180,7 @@ from typing import Union
 
 app = FastAPI(docs_url=None, redoc_url=None, openapi_url=None)
 app.mount("/css", StaticFiles(directory="./css"), name="static")
-app.mount(f"/{REDIRECT_PATH}", StaticFiles(directory="./blog", html=True), name="static")
+app.mount("/delight", StaticFiles(directory="./blog", html=True), name="static")
 app.add_middleware(GZipMiddleware, minimum_size=1000)
 
 from fastapi.templating import Jinja2Templates
@@ -196,7 +196,7 @@ def home(response: Response, request: Request, yuki: Union[str] = Cookie(None)):
         return template("home.html", {"request": request})
     
     print(check_cookie(yuki))
-    return RedirectResponse("/siawaseok")
+    return RedirectResponse("/delight")
 
 @app.get('/watch', response_class=HTMLResponse)
 def video(v:str,response: Response,request: Request,yuki: Union[str] = Cookie(None),proxy: Union[str] = Cookie(None)):
