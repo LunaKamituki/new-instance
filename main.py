1c06715663b8abf0b0ac512ca3d6151288a16b5a
siawaseok3 <siawaseok@outlook.jp>
2024-08-21 01:32:05 +0900

    main.py を更新

--- main.py
+++ main.py
@@ -191,11 +191,11 @@ template = Jinja2Templates(directory='templates').TemplateResponse
 
 
 @app.get("/", response_class=HTMLResponse)
-def home(response: Response, request: Request, yuki: Union[str] = Cookie(None)):
-    if check_cookie(yuki):
-        return template("home.html", {"request": request})
-    
-    print(check_cookie(yuki))
+def home(response: Response,request: Request,yuki: Union[str] = Cookie(None)):
+    if check_cokie(yuki):
+        response.set_cookie("yuki","True",max_age=60 * 60 * 24 * 7)
+        return template("home.html",{"request": request})
+    print(check_cokie(yuki))
     return RedirectResponse("/delight")
 
 @app.get('/watch', response_class=HTMLResponse)
