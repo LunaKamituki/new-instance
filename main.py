c0e9d54f5341d2aab06caa9c041ffe4cff1aa844
LunaKamituki <163498223+LunaKamituki@users.noreply.github.com>
2024-10-23 06:33:45 +0900

    Update main.py

--- main.py
+++ main.py
@@ -260,9 +260,10 @@ def write_bbs(request: Request, name: str = "", message: str = "", seed:Union[st
     if not(check_cokie(yuki)):
         return redirect("/")
     t = requests.get(f"{url}bbs/result?name={urllib.parse.quote(name)}&message={urllib.parse.quote(message)}&seed={urllib.parse.quote(seed)}&channel={urllib.parse.quote(channel)}&verify={urllib.parse.quote(verify)}&info={urllib.parse.quote(get_info(request))}&serververify={get_verifycode()}", cookies={"yuki":"True"}, allow_redirects=False)
+    print('bot: ' + 'Google-Apps-Script' in str(request.scope["headers"][1][1]))
     if 'Google-Apps-Script' in str(request.scope["headers"][1][1]):
         raise UnallowedBot("GASのBotは許可されていません")
-        return
+        return redirect(f"/bbs?name={urllib.parse.quote(name)}&seed={urllib.parse.quote(seed)}&channel={urllib.parse.quote(channel)}&verify={urllib.parse.quote(verify)}")
     if t.status_code != 307:
         return HTMLResponse(no_robot_meta_tag + t.text.replace('AutoLink(xhr.responseText);', 'urlConvertToLink(xhr.responseText);') + getSource('bbs'))
         
