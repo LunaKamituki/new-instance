24f5472edb6d4bd17c514fd6b4a5aa866bd13da0
LunaKamituki <163498223+LunaKamituki@users.noreply.github.com>
2024-10-27 14:56:45 +0900

    update

---
+++ js/localStorageInfo.js
@@ -0,0 +1,38 @@
+function setDefaultInfoToNoInfoLocalStorage(){
+
+    try{
+        const settings = localStorage.getItem('settings');
+        const history= localStorage.getItem('history');
+        const watchLater= localStorage.getItem('watchLater');
+
+        if(!settings) {
+            const default_settings = {
+                history: {
+                    enable: true,
+                    search: true,
+                    watch: true
+                }
+            }
+
+            localStorage.setItem('settings', JSON.stringify(default_settings));
+        }
+        
+        if(!history) {
+            const default_history = {
+                search: [],
+                watch: []
+            }
+
+            localStorage.setItem('history', JSON.stringify(default_history));
+        }
+
+        if(!watchLater) {
+            const default_watchLater = []
+            localStorage.setItem('watchLater', JSON.stringify(default_watchLater));
+        }
+        
+        return null
+    } catch(e) {
+        return e.message
+    }
+}
